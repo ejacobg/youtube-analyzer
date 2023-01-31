3876f9526eb0cb315b11836d3bdfbb98d2670279
GMor10sen <58446889+GMor10sen@users.noreply.github.com>
2023-01-30 17:03:18 -0800

    Added some comments

--- read.py
+++ read.py
@@ -7,10 +7,13 @@ sc = SparkContext('local')
 spark = SparkSession(sc)
 
 def read_file(file):
+    #read data between the columns 
     df = spark.read.csv(file, sep='\t')
-
+    
+    #renamed columns
     df = df.select(*df.columns[:9], array(df.columns[9:])).toDF('videoID', 'uploader', 'age', 'category', 'length', 'views', 'rate', 'ratings', 'comments', 'relatedIDs')
 
+    #read data of each columns as string and cast them to right datatype 
     return (df.withColumn('age', df.age.cast('int'))
         .withColumn('length', df.length.cast('int'))
         .withColumn('views', df.views.cast('int'))
@@ -19,8 +22,10 @@ def read_file(file):
         .withColumn('comments', df.comments.cast('int'))
         .withColumn('links', size('relatedIDs')))
 
+#getting the data from website download 
 df = read_file(['./0222/0.txt', './0222/1.txt'])
 
+#show 
 df.show(1)
 
 related = df.select(df.videoID, explode(df.relatedIDs).alias('relatedID'))
