1c4fa7032d484325b2ff63c2efb418a2719c0de2
Ean Jacob Gayban <ejacobg@outlook.com>
2023-02-09 17:00:46 -0800

    menu cleanup

--- menu.py
+++ menu.py
@@ -1,119 +1,94 @@
 import os
 
-
-def menu_options(command):
-    if command == "D":
-        path = get_file("Input output directory:")
-		 
-        print("Writing to ", path)
-		
-        print("[Call function to degrees]")
-
-        print("Finished")
-		
-        menu_display()
-
-    if command == "C": 
-        print("Filter data:")
-		
-
-        user_input = False
-        subcommand = " "
-        while not (subcommand == "G" or subcommand == "E" or subcommand == "B"):
-            print("===Menu===\n [G]enerate statistics\n [E]xport\n [B]ack")
-            subcommand = input("Enter Command:")
-           
-            if (subcommand == "G" or subcommand == "B" or subcommand == "E"):
-                Option_C(subcommand)
-      
-            else:
-                print("Not a valid command, try again please.")
-        
-
-def Option_C(command, user_input):
-
-    response = " "
-
-    if(command == "G"):
-        
-        while not (response == "M"):
-            columns = input("Select columns to aggregate on:")
-            # [columns...]
-            print("Numeric column <column> requires bucketing. Enter buckets: <idk how to format this yet>")
-            
-            #   repeat above for all numeric columns]
-            
-            print("Distribution of videos according to above criteria:")
-            #   [Spark output]
-            response = input("\nMenu:\n [R]epeat\n [M]ain Menu\n")
-
-            
-    if(command == "R"):
-        print("Export")
-        while not (response == "S" or response == "A" ):
-            
-            response = input("Menu:\n [S]elected Columns\n  [A]ll Columns\n\n")
-		     
-            path = get_file("Input output directory:")
-            print("Writing to ", path)
-            
-            if response == "S" :
-                columns = input("Select columns:")
-            
-            elif response == "A":
-                print("All columns")
-                #[Spark stuff]
-
-            else:
-                print("Not a valid command, try again please.")
-		        
-    print("Finished...")
-     
 def main():
-    #get_file("Enter input file(s) path:")
-    menu_display()
+    # get_file("Enter input file(s) path:")
+    menu()
 
 
+def menu():
+    # Read given file(s).
+    df = input_file()
 
-def get_file(custom_text):
-       
-    file_exist = False
-    while (file_exist == False):
-
-        directory = input(custom_text)
-
-        if os.path.isdir(directory):
-            print("Reading Files...")
-            file_exist = True
-            return directory
-            
-        else:
-            print(f"{directory} does not exist, please try again")
-            
-
-
-
-def menu_display(): 
-
+    # Display menu.
     command = " "
-
-    # repeat menu over and over until valid option is shown 
-    while not (command == "E" ):
-        print( "\n====Menu====\n [D]egree Distribution\n [C]ategorized Statistics\n [R]estart\n [E]xit\n\n")
+    while True:
+        print(
+            "====Menu====\n [D]egree Distribution\n [C]ategorized Statistics\n [R]estart\n [E]xit"
+        )
 
         command = input("Enter Command:")
-        
-        # if command valid, go to option 
-        if (command == "D" or command == "C" or command == "R"):
-            menu_options(command)
 
-        if (command == "E"):
-            exit_variable = True
-
-        # otherwise loop again 
+        if command == "D":
+            degree_distribution()
+        elif command == "C":
+            categorized_statistics()
+        elif command == "R":
+            df = input_file()
+        elif command == "E":
+            break
         else:
-            print("Not a valid command, try again please.")
+            print("Not a valid command, please try again.")
 
+def input_file():
+    file = input("Enter input file(s): ")
+    # Pass file data into Spark and return Dataframe.
+    # Also return a second argument indicating success?
+    return
+
+def degree_distribution():
+    dir = input("Output directory:")
+    # Calculate degree stuff...
+    print("Writing to ", dir, "...", sep="")
+    # degrees.write.csv(...)
+    # Don't need to return anything?
+    print("Finished.")
+
+def categorized_statistics():
+    print("Filter data:")
+    # Not sure what options I want yet
+    # Just take a single SQL query to filter with?
+
+    while True:
+        command = input("[G]enerate statistics or [E]xport: ")
+        if command == "G":
+            generate_statistics()
+            break
+        elif command == "E":
+            export()
+            break
+        else:
+            print("Invalid input.")
+    return
+
+def generate_statistics():
+    print("Select columns to aggregate on:")
+    # Print columns, marking them if they have been selected already
+    # Alternatively, just remove a column from the list if it has been selected
+
+    # Get column choice
+    column = input("> ")
+
+    # If column is numeric
+    print("Numeric column", column, "requires bucketing.")
+    buckets = input("Enter buckets: ")
+
+    # Save all selected columns/buckets in a list or something
+
+    # Run Spark calculation
+
+    print("Distribution of videos according to above criteria:")
+
+    return 
+
+def export():
+    print("Export\n\t[S]elected Columns\n\t[A]ll Columns\n")
+    command = input("> ")
+
+    dir = input("Output directory:")
+    print("Writing to ", dir, "...", sep="")
+    # Spark stuff
+    print("Finished.")
+    return
 
 if __name__ == "__main__":
     main()
